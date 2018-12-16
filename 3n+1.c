4d5b2f78c1ffd73622d52b0457c46cfde904fa6b
limcheyyong <45914390+limcheyyong@users.noreply.github.com>
2018-12-16 23:26:17 +0800

    Create 3n+1.c

---
+++ 3n+1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+    int n ;
+    int counter = 1;
+    scanf("%d",&n);
+    while(n != 1){
+        if (n % 2 != 0){
+            n = 3 * n + 1;
+        else {
+            n = n / 2;
+        }
+        counter = counter + 1;
+    }
+    printf("%d",counter);
+    return 0;
+}
