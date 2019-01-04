2a050cfeb6f33f34b9a1d3ed2011296125661ef2
wanjuyangg <41325824+wanjuyangg@users.noreply.github.com>
2019-01-04 21:29:58 +0800

    新增隊伍

--- 01.insert.php
+++ 01.insert.php
@@ -7,11 +7,11 @@
 
 <body>
 
-<p>insert new message</p>
+<p>insert new team</p>
 <hr />
 <?php
 require('dbconfig.php');
-
+require("loginModel.php");
 // $title=$_POST['title'];
 // $msg=$_POST['msg'];
 // $name=$_POST['myname'];
@@ -19,14 +19,37 @@ require('dbconfig.php');
 $tname =$_POST['team'];
 $player =$_POST['player'];
 $status = '等待中';
+$uname = getCurrentUserName();
 if ($tname) {
-	// if($player == 'factory')
+	if($player == 'factory') {
+		$sql = "insert into team (tname,factory,status) values (?,?,?)";
+		$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+		mysqli_stmt_bind_param($stmt, "sss", $tname, $uname,$status); //bind parameters with variables
+		mysqli_stmt_execute($stmt);  //執行SQL
+		echo "team added.";
+	}
+	// if($player == 'factory') {
+	// 	$sql = "insert into team (tname,status) values (?,?)";
+	// 	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	// 	mysqli_stmt_bind_param($stmt, "ss", $tname,$status); //bind parameters with variables
+	// 	mysqli_stmt_execute($stmt);  //執行SQL
+	// 	echo "team added.";
+	// }
+	// if($player == 'factory') {
+	// 	$sql = "insert into team (tname,status) values (?,?)";
+	// 	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	// 	mysqli_stmt_bind_param($stmt, "ss", $tname,$status); //bind parameters with variables
+	// 	mysqli_stmt_execute($stmt);  //執行SQL
+	// 	echo "team added.";
+	// }
+	// if($player == 'factory') {
+	// 	$sql = "insert into team (tname,status) values (?,?)";
+	// 	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	// 	mysqli_stmt_bind_param($stmt, "ss", $tname,$status); //bind parameters with variables
+	// 	mysqli_stmt_execute($stmt);  //執行SQL
+	// 	echo "team added.";
+	// }
 
-	$sql = "insert into team (tname,status) values (?,?)";
-	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
-	mysqli_stmt_bind_param($stmt, "ss", $tname,$status); //bind parameters with variables
-	mysqli_stmt_execute($stmt);  //執行SQL
-	echo "team added.";
 } else {
 	echo "empty title, cannot insert.";
 }
