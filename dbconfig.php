8a7b627d382223e6bd78f2c6a8f2fe6c8d0df299
s105213049 <s105213049@mail1.ncnu.edu.tw>
2019-01-09 00:06:37 +0800

    session['uid'] = NULL

--- dbconfig.php
+++ dbconfig.php
@@ -1,16 +1,16 @@
-<?php
+﻿<?php
 session_start();
 
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$dbName = 'beergame';
+$dbName = 'team3';
 $db = mysqli_connect($host, $user, $pass, $dbName) or die('Error with MySQL connection'); //跟MyMSQL連線並登入
 mysqli_query($db,"SET NAMES utf8"); //選擇編碼
 
 function checkLogin() {
     // echo $_SESSION["uid"];
-    if ( ! isset($_SESSION["uid"]) or $_SESSION["uid"] <= 0) {
+    if ( ! isset($_SESSION["uid"]) or $_SESSION["uid"] = NULL) {
             header("Location: loginView.php");
     }
 }
