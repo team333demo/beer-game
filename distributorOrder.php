6935fff0e876e8de20863e4bdd491b87c2a1b426
limcheyyong <45914390+limcheyyong@users.noreply.github.com>
2019-01-08 23:00:23 +0800

    更新期限

--- distributorOrder.php
+++ distributorOrder.php
@@ -4,23 +4,25 @@ require_once("dOrderView.php");
 $period=$_POST["curr"];
 $opr = $_POST["opr"];
 $num = (int)$_POST["num"];
+$Tid = $_REQUEST["Tid"];
+echo $Tid;
 switch($opr){
     case "reset":
         init();		
         break;
     case "play":
 		update($num,$period,$Tid);
-		if ($period == 3)	{
+		if ($period == 10)	{
 			header("Location: endView.php");
 		}else{
 		addOrder($period+1,$Tid);        
 		updatearrival($period,$Tid);
 		updatesales($period,$Tid);
-			
+			header("Location: distributor.php?Tid=".$Tid);
 		}
 		
  
 		break;
 }
-header("Location: distributor.php?Tid=".$Tid);
+
 ?>
